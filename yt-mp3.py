d7deaeba3f71ccbd6ae31b3a5666a5f6e0bdb3f9
Soldat Fantom <soldatfantom29337@gmail.com>
2024-10-23 15:59:10 -0400

    Add files via upload

--- yt-mp3.py
+++ yt-mp3.py
@@ -5,18 +5,14 @@ import yt_dlp
 from pydub import AudioSegment
 import os
 import threading
-def browse_folder():
-    # Ouvre l'explorateur de fichiers pour sélectionner un dossier
-    initial_dir = os.path.expanduser("~")  # Chemin par défaut au dossier utilisateur
-    selected_folder = filedialog.askdirectory(initialdir=initial_dir)
 
-    # Si un dossier est sélectionné
+def browse_folder():
+    selected_folder = filedialog.askdirectory(initialdir=os.path.expanduser("~/Music"))
     if selected_folder:
-        folder_entry.delete(0, tk.END)  # Efface l'ancienne entrée
-        folder_entry.insert(0, selected_folder)  # Insère le nouveau chemin
+        folder_entry.delete(0, tk.END)
+        folder_entry.insert(0, selected_folder)
 
 def create_new_folder_dialog():
-    # Crée une nouvelle fenêtre contextuelle pour entrer le nom du nouveau dossier
     new_folder_window = tk.Toplevel(root)
     new_folder_window.title("Créer un nouveau dossier")
 
@@ -25,43 +21,45 @@ def create_new_folder_dialog():
     new_folder_name_entry.pack(padx=10, pady=10)
 
     def create_folder():
-        # Récupère le chemin du dossier sélectionné
         base_folder = folder_entry.get()
-        new_folder_name = new_folder_name_entry.get().strip()  # Nom du nouveau dossier
-
-        if base_folder and new_folder_name:  # Vérifie si les deux champs sont remplis
+        new_folder_name = new_folder_name_entry.get().strip()
+        if base_folder and new_folder_name:
             new_folder_path = os.path.join(base_folder, new_folder_name)
             try:
-                os.makedirs(new_folder_path, exist_ok=True)  # Crée le nouveau dossier
+                os.makedirs(new_folder_path, exist_ok=True)
                 log_console.insert(tk.END, f"Dossier créé : {new_folder_path}\n")
-                folder_entry.delete(0, tk.END)  # Efface l'ancienne entrée
-                folder_entry.insert(0, new_folder_path)  # Insère le chemin du nouveau dossier
-                new_folder_window.destroy()  # Ferme la fenêtre contextuelle
+                folder_entry.delete(0, tk.END)
+                folder_entry.insert(0, new_folder_path)
+                new_folder_window.destroy()
             except Exception as e:
                 log_console.insert(tk.END, f"Erreur lors de la création du dossier : {str(e)}\n")
         else:
-            log_console.insert(tk.END, "Erreur : Veuillez sélectionner un dossier et entrer un nom de dossier.\n")
+            log_console.insert(tk.END, "Erreur : Veuillez entrer un nom de dossier.\n")
 
     create_button = tk.Button(new_folder_window, text="Créer", command=create_folder)
     create_button.pack(padx=10, pady=10)
+
+def update_log_console(message):
+    log_console.insert(tk.END, message)
+    log_console.see(tk.END)
+
+def show_messagebox(title, message):
+    messagebox.showinfo(title, message)
+
 def download_and_convert_to_mp3():
     url = url_entry.get()
-    if not url:
-        log_console.insert(tk.END, "Erreur: Veuillez entrer une URL valide.\n")
+    download_folder = folder_entry.get()
+
+    if not url or not download_folder:
+        update_log_console("Erreur: Veuillez entrer une URL valide et un dossier de destination.\n")
         return
 
-    # Configuration de la barre de progression
-    progress_bar["value"] = 0
-    progress_bar["maximum"] = 100
+    download_progress_bar["value"] = 0
+    download_progress_bar["maximum"] = 100
+    conversion_progress_bar["value"] = 0
+    conversion_progress_bar["maximum"] = 100
 
     def run_download():
-        download_folder = folder_entry.get()
-
-        if not download_folder:
-            log_console.insert(tk.END, "Erreur: Veuillez sélectionner un dossier de destination.\n")
-            return
-
-        # Options pour télécharger au format webm
         options = {
             'format': 'bestaudio/best',
             'outtmpl': f'{download_folder}/%(title)s.%(ext)s',
@@ -69,44 +67,61 @@ def download_and_convert_to_mp3():
         }
 
         try:
+            # Extraire les informations de la vidéo avant de commencer le téléchargement
             with yt_dlp.YoutubeDL(options) as ydl:
-                log_console.insert(tk.END, "Téléchargement en cours...\n")
+                root.after(0, update_log_console, "Début de l'opération ...\n")
                 info_dict = ydl.extract_info(url, download=False)
                 title = info_dict.get('title', None)
+
+                # Afficher le nom de la musique dans la console avant de télécharger
+                root.after(0, update_log_console, f"Nom de la musique : {title}\n")
+                root.after(0, update_log_console, "Téléchargement en cours...\n")
+
+                # Télécharger la vidéo
                 ydl.download([url])
         except Exception as e:
-            log_console.insert(tk.END, f"Erreur lors du téléchargement: {str(e)}\n")
+            root.after(0, update_log_console, f"Erreur lors du téléchargement: {str(e)}\n")
             return
 
-        # Conversion du fichier webm en MP3
         webm_filepath = os.path.join(download_folder, f'{title}.webm')
         mp3_filepath = os.path.join(download_folder, f'{title}.mp3')
 
         try:
             audio = AudioSegment.from_file(webm_filepath, format="webm")
-            log_console.insert(tk.END, f"Conversion en MP3 ....: {mp3_filepath}\n")
-            audio.export(mp3_filepath, format="mp3")
-            os.remove(webm_filepath)  # Supprimer le fichier WebM après la conversion
-            log_console.insert(tk.END, f"Conversion en MP3 terminée: {mp3_filepath}\n")
-            messagebox.showinfo("Succès", f"Téléchargement et conversion en MP3 terminés.\nFichier MP3 enregistré à : {mp3_filepath}")
-        except Exception as e:
-            log_console.insert(tk.END, f"Erreur lors de la conversion en MP3: {str(e)}\n")
+            root.after(0, update_log_console, f"Conversion en MP3 en cours: {mp3_filepath}\n")
 
-    # Fonction de mise à jour de la barre de progression
+            # Conversion segmentée et mise à jour de la barre de progression
+            segment_duration_ms = 1000  # Segment de 1 seconde
+            total_duration_ms = len(audio)  # Durée totale en millisecondes
+            progress_step = 100 / (total_duration_ms // segment_duration_ms)
+
+            converted_audio = AudioSegment.silent(duration=0)
+            for start in range(0, total_duration_ms, segment_duration_ms):
+                end = min(start + segment_duration_ms, total_duration_ms)
+                segment = audio[start:end]
+                converted_audio += segment
+
+                conversion_progress_bar["value"] += progress_step
+                root.update_idletasks()  # Mise à jour de l'interface
+
+            converted_audio.export(mp3_filepath, format="mp3")
+            os.remove(webm_filepath)
+            root.after(0, update_log_console, f"Conversion en MP3 terminée: {mp3_filepath}\n")
+            root.after(0, show_messagebox, "Succès", f"Conversion terminée.\nFichier MP3 sauvegardé à : {mp3_filepath}")
+        except Exception as e:
+            root.after(0, update_log_console, f"Erreur lors de la conversion en MP3: {str(e)}\n")
+
     def hook(d):
         if d['status'] == 'downloading':
             percent = d['downloaded_bytes'] / d['total_bytes'] * 100
-            progress_bar["value"] = percent
-            root.update_idletasks()  # Met à jour l'interface
+            download_progress_bar["value"] = percent
+            root.update_idletasks()
 
         if d['status'] == 'finished':
-            progress_bar["value"] = 100
-            log_console.insert(tk.END, "Téléchargement terminé.\n")
+            root.after(0, update_log_console, "Téléchargement terminé.\n")
 
-    # Exécute le téléchargement dans un thread séparé pour ne pas bloquer l'interface
     threading.Thread(target=run_download).start()
 
-# Création de l'interface utilisateur
 # Création de l'interface utilisateur
 root = tk.Tk()
 root.title("Téléchargeur et Convertisseur MP3 YouTube")
@@ -117,18 +132,16 @@ url_label.pack()
 url_entry = tk.Entry(root, width=50)
 url_entry.pack()
 
-# Créer un frame pour contenir l'entrée de texte et le bouton
 folder_frame = tk.Frame(root)
-folder_frame.pack(pady=5)  # Ajoute un peu d'espace vertical autour du cadre
+folder_frame.pack(pady=5)
 
 folder_label = tk.Label(folder_frame, text="Destination :")
 folder_label.grid(row=0, column=0)
 
-# Nouveau bouton pour créer un dossier
 create_button = tk.Button(folder_frame, text="Créer Dossier", command=create_new_folder_dialog)
 create_button.grid(row=0, column=3)
 
-folder_entry = tk.Entry(folder_frame, width=30)  # On diminue la largeur de l'entrée pour laisser de la place au bouton
+folder_entry = tk.Entry(folder_frame, width=30)
 folder_entry.grid(row=0, column=1, padx=5)
 
 folder_button = tk.Button(folder_frame, text="Parcourir", command=browse_folder)
@@ -137,13 +150,19 @@ folder_button.grid(row=0, column=2)
 download_button = tk.Button(root, text="Télécharger et Convertir en MP3", command=download_and_convert_to_mp3)
 download_button.pack()
 
-# Barre de progression
-progress_bar = ttk.Progressbar(root, length=300, mode='determinate')
-progress_bar.pack(pady=10)
+# Barre de progression pour le téléchargement
+download_progress_bar = ttk.Progressbar(root, length=300, mode='determinate')
+download_progress_bar.pack(pady=5)
+download_label = tk.Label(root, text="Téléchargement")
+download_label.pack()
 
-# Console pour afficher les logs
+# Barre de progression pour la conversion
+conversion_progress_bar = ttk.Progressbar(root, length=300, mode='determinate')
+conversion_progress_bar.pack(pady=5)
+conversion_label = tk.Label(root, text="Conversion")
+conversion_label.pack()
+
 log_console = scrolledtext.ScrolledText(root, height=10, width=60, bg="black", fg="white")
 log_console.pack(pady=10)
 
 root.mainloop()
-

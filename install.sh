6948708b59210755634f99dadca1f64bcd3972ff
Soldat Fantom <soldatfantom29337@gmail.com>
2024-10-23 12:23:27 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-exec 2>/dev/null
+
 # Vérifier si le script est exécuté avec les droits d'administrateur
 if [ "$EUID" -ne 0 ]; then
     echo "Veuillez exécuter ce script avec sudo"
@@ -12,10 +12,13 @@ echo "Installation de yt-mp3"
 # Installer les dépendances nécessaires
 
 # Copier des fichiers dans les répertoires appropriés
-tar -xzvf dist.tar.gz
+pip install yt-dlp
+pip install pydub
+pip install pyinstaller
+pyinstaller --onefile yt-mp3.py
 cp dist/yt-mp3 /usr/local/bin/yt-mp3
 chmod +x /usr/local/bin/yt-mp3
 echo "Installation terminée !"
 cd ..
 sudo rm -rf Yt-mp3
-rm output.log
+
